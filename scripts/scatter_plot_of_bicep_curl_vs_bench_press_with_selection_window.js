14997b3b74da21577612b34cb1390c58e824f6b1
p3rciv3l <avikrish001@gmail.com>
2024-04-19 18:52:58 -0400

    fourth commit

--- scripts/scatter_plot_of_bicep_curl_vs_bench_press_with_selection_window.js
+++
@@ -1,9 +0,0 @@
-    const spec_scatter_plot_of_bicep_curl_vs_bench_press_with_selection_window = {
-        $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-        ,
-    };
-
-    vegaEmbed('#scatter_plot_of_bicep_curl_vs_bench_press_with_selection_window', 
-    spec_scatter_plot_of_bicep_curl_vs_bench_press_with_selection_window, 
-    { "actions": false }).catch(console.error);
-
